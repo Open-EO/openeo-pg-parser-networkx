d3ad420926f7af223bc87d300c23cfb10a74b65d
Lukas Weidenholzer <lukas.weidenholzer@eodc.eu>
2022-07-08 15:33:13 +0200

    use unflattener

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PG Parser
 
+## Pydantic Typing
+
+Pydantic datamodels were downloaded using:
+`poetry run datamodel-codegen --url https://openeo.org/documentation/1.0/developers/api/assets/pg-schema.json --output eodc_pg_parser/models/pg_schema.py --input-file-type jsonschema`
+
+
+
+
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,10 +1,27 @@
-from eodc_pg_parser.models import ProcessGraph, ProcessNode
+from eodc_pg_parser.pg_schema import ProcessNode
+from openeo.internal.process_graph_visitor import ProcessGraphVisitor, ProcessGraphUnflattener
 
 from unittest import TestCase
-
+import json
 
 class PGTester(TestCase):
-    def test_json_parsing(self):
-        pg = ProcessGraph.parse_file("tests/data/fit_rf_pg_0.json")
-        print(pg)
-            
+    def test_graph_visitor(self):
+        pg_1 = json.load(open("tests/data/fit_rf_pg_0.json", mode="r"))
+
+        pg_visitor = ProcessGraphVisitor()
+        graph = pg_visitor.accept_process_graph(pg_1["process_graph"])
+
+    def test_ProcessGraphUnflattener(self):
+        flat_graph = json.load(open("tests/data/fit_rf_pg_0.json", mode="r"))
+
+        pg_unflattener = ProcessGraphUnflattener(flat_graph)
+        graph = pg_unflattener.unflatten(flat_graph["process_graph"])
+        print(graph)
+
+    def test_unflattened_data_model(self):
+        flat_graph = json.load(open("tests/data/fit_rf_pg_0.json", mode="r"))
+        pg_unflattener = ProcessGraphUnflattener(flat_graph)
+        graph = pg_unflattener.unflatten(flat_graph["process_graph"])
+        parsed_graph = ProcessNode.parse_obj(graph)
+        k = [arg for arg in parsed_graph.arguments]
+        print("")
