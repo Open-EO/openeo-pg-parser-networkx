6a6f9482f090161842896957db4e9390e1baedc4
Lukas Weidenholzer <lukas.weidenholzer@eodc.eu>
2022-07-12 14:31:40 +0200

    add explicit test for pydantic types

--- tests/test_pg_parser.py
+++ tests/test_pg_parser.py
@@ -4,6 +4,8 @@ import pytest
 import json
 from pathlib import Path
 
+from eodc_pg_parser.pg_schema import ProcessArgument, ProcessGraph, ProcessNode, ResultReference
+
 TEST_DATA_DIR = Path("tests/data/")
 
 # Run the tests across all these process graphs
@@ -25,6 +27,18 @@ def test_unflattening(flat_process_graph, nested_process_graph):
     print(nested_process_graph)
 
 def test_data_model_parsing(flat_process_graph, openeo_graph):
-
     print(openeo_graph)
         
+def test_data_types_explicitly():
+    flat_process_graph = json.load(open(TEST_DATA_DIR / "fit_rf_pg_0.json", mode="r"))
+    nested_process_graph = OpenEOProcessGraph._unflatten_raw_process_graph(flat_process_graph)
+    parsed_process_graph = OpenEOProcessGraph._parse_datamodel(nested_process_graph)
+    assert isinstance(parsed_process_graph, ProcessGraph)
+    assert isinstance(parsed_process_graph.process_graph["root"], ProcessNode)
+    assert isinstance(parsed_process_graph.process_graph["root"].arguments["model"], ProcessArgument)
+    assert isinstance(parsed_process_graph.process_graph["root"].arguments["model"].__root__, ResultReference)
+    assert isinstance(parsed_process_graph.process_graph["root"].arguments["model"].__root__.node, ProcessNode)
+
+
+    print("")
+
