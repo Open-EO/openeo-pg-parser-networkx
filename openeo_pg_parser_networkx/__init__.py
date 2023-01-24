5e50ea78a7957ac916355ce111d858dd9c19e920
Lukas Weidenholzer <lukas.weidenholzer@eodc.eu>
2023-01-24 16:15:20 +0100

    bump version to 2023.1.0

--- openeo_pg_parser_networkx/__init__.py
+++ openeo_pg_parser_networkx/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2022.12.1"
+__version__ = "2023.1.0"
 
 from openeo_pg_parser_networkx.graph import OpenEOProcessGraph
 from openeo_pg_parser_networkx.process_registry import ProcessRegistry
