f98d81a99aefa454fb1fac1711017a0eed2e0a34
Lukas Weidenholzer <lukas.weidenholzer@eodc.eu>
2023-03-13 10:39:13 +0100

    fix precommit

--- tests/test_pg_parser.py
+++ tests/test_pg_parser.py
@@ -213,7 +213,7 @@ def test_temporal_intervals(get_process_graph_with_args):
     assert isinstance(second_interval, TemporalInterval)
     assert isinstance(second_interval.start, Date)
     assert isinstance(second_interval.end, Year)
-    
+
     assert isinstance(third_interval, TemporalInterval)
     assert isinstance(second_interval.start, Date)
 
