873cdd73d7ac49fb95ecb9e3b746ae1f3cd6983e
Lukas Weidenholzer <17790923+LukeWeidenwalker@users.noreply.github.com>
2023-03-13 11:44:19 +0100

    release: 2023.3.0 (#32)

--- openeo_pg_parser_networkx/__init__.py
+++ openeo_pg_parser_networkx/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2023.1.2"
+__version__ = "2023.3.0"
 
 from openeo_pg_parser_networkx.graph import OpenEOProcessGraph
 from openeo_pg_parser_networkx.process_registry import ProcessRegistry
