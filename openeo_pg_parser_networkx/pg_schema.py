abbbb0a891c9c276e304315a81e0c50de4d42fa8
Michele Claus <michele.claus@eurac.edu>
2025-12-15 13:28:15 +0000

    Fix int parsing

--- openeo_pg_parser_networkx/pg_schema.py
+++ openeo_pg_parser_networkx/pg_schema.py
@@ -90,6 +90,7 @@ class ProcessNode(BaseModel, arbitrary_types_allowed=True):
                 TemporalIntervals,
                 # GeoJson, disable while https://github.com/developmentseed/geojson-pydantic/issues/92 is open
                 Time,
+                int,
                 float,
                 bool,
                 list,
